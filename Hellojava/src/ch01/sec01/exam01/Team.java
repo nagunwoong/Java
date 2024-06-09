a97e44eaf55e4f85a5709e99b1a3beadc8d1bc77
NAGUNWOONG <https://github.com/nagunwoong/Test.git>
2024-06-10 01:32:24 +0900

    0610

--- Hellojava/src/ch01/sec01/exam01/Team.java
+++ Hellojava/src/ch01/sec01/exam01/Team.java
@@ -7,7 +7,7 @@ public class Team {
 		String[] classgroup = {"김민준", "나건웅", "이재민" };
 		for(int i = 0; i < classgroup.length; i++) {
 			String classnum = classgroup[i];
-			System.out.println( classnum + "상담을 받음");
+			System.out.println( classnum + "이 상담을 받음");
 		}
 		
 	}
