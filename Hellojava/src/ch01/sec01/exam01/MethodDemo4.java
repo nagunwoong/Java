768249ba103cc51c9c9c0654dd5be9abfb0aad56
NAGUNWOONG <gallawer@naver.com>
2024-06-10 20:12:10 +0900

    0610

---
+++ Hellojava/src/ch01/sec01/exam01/MethodDemo4.java
@@ -0,0 +1,20 @@
+package ch01.sec01.exam01;
+
+public class MethodDemo4 {
+									//매게 변수
+									//파라미터
+	public static void numbering(int amg) {
+		
+		int i = 0;
+		while (i < amg) {
+			System.out.println(i);
+			i++;
+		}
+		
+		
+	}
+	public static void main(String[] args) {
+		numbering(4);
+				//인자, 아귀먼트
+	}
+}
