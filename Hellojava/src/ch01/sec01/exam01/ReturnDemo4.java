6a27eb7d4c6d85a9f4c35f574cd0d7a976a167c1
NAGUNWOONG <gallawer@naver.com>
2024-06-10 20:37:29 +0900

    0610

---
+++ Hellojava/src/ch01/sec01/exam01/ReturnDemo4.java
@@ -0,0 +1,17 @@
+package ch01.sec01.exam01;
+
+public class ReturnDemo4 {
+
+	public static String[] getMembers() {
+		String[] member = {"나건웅", "김민준", "이재민" };
+		return member;
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		String[]member = getMembers();
+		System.out.println(member);
+	}
+
+}
