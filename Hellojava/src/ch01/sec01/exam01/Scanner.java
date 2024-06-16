6f19bdff97dff704422ff4a6743fb542eea81ef6
NAGUNWOONG <gallawer@naver.com>
2024-06-16 20:11:47 +0900

    0616

---
+++ Hellojava/src/ch01/sec01/exam01/Scanner.java
@@ -0,0 +1,19 @@
+package ch01.sec01.exam01;
+
+public class Scanner {
+
+	public static void main(String[] args) {
+		
+		
+		int a = 1;
+		int b = 2;
+		
+		if(a  < 0 && b > 0) {
+			System.out.println("같습니다");
+		} else {
+			System.out.println("틀립니다");
+		}
+		
+	}
+
+}
